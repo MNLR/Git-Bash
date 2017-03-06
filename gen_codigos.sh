aabe0c6c210da55062873688d039a98bf2c6b801
MNLR <bstnss@hotmail.com>
2017-03-06 15:04:17 +0100

    gen_codigos.sh ahora sustituye caracteres especiales. Reorganizado directorio de trabajo. Anadido .gitignore, ahora se ignora la carpeta temp, donde se descargan los .zip

--- gen_codigos.sh
+++ gen_codigos.sh
@@ -6,6 +6,7 @@ rm cod_ccaa_bruto
 
 cat cod_ccaa | awk '{print $1}' > cod_ccaa_n
 cat cod_ccaa | cut -d' ' -f2-10 | sed 's/ /_/g'  > cod_ccaa_c
+rm cod_ccaa
 
 # Obtencion de los codigos de provincia
 w3m http://www.ine.es/daco/daco42/codmun/cod_provincia.htm > cod_p_bruto
@@ -13,6 +14,13 @@ linea0=$(cat cod_p_bruto | grep -n "provincias con sus" | cut -d ':' -f 1)
 tail -n +$linea0 cod_p_bruto | head -n 22 | grep [0-9] | sed 's/í/i/g' | sed 's/ó/o/g' | sed 's/ñ/n/g' | sed 's/á/a/g' | sed 's/é/e/g' > cod_p
 
 rm cod_p_bruto
-cat cod_p | grep -Eo "[0-9]+" >> cod_p_n
-cat cod_p | grep -Eo "[^0-9]+" >> cod_p_p
+
+# Separamos ordenadamente los codigos y provincias
+cat cod_p | grep -Eo "[0-9]+" > cod_p_n
+cat cod_p | grep -Eo "[^0-9]+" > cod_p_paux
+rm cod_p
+
+#cat cod_p_paux | sed "s/ /_/g" | sed "s/,/_/g" > cod_p_p
+cat cod_p_paux | awk '{print $1 $2 $3 $4}' | sed "s/,/_/g" | sed "s/\//_/g" | sed "s/è/e/g" | sed "s/Á/A/g" > cod_p_p
+
 
