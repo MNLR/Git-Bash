40f22116ad4dd240e5b3b25b37c21a6e39d79528
MNLR <bstnss@hotmail.com>
2017-04-11 14:11:02 +0200

    Error tipografico corregido

--- gen_codigos.sh
+++ gen_codigos.sh
@@ -18,14 +18,13 @@ linea0=$(cat cod_pp_bruto | grep -n "provincias con sus" | cut -d ':' -f 1)
 tail -n +$linea0 cod_pp_bruto | head -n 22 | grep [0-9] > cod_pp_preprocesado
 
 cat cod_pp_preprocesado | sed 's/á/a/g; s/é/e/g; s/í/i/g; s/ó/o/g; s/ú/u/g; s/ñ/n/g; s/,//g; s/Á/A/g; s/É/E/g ; s/Í/I/g ; s/Ó/O/g ; s/Ú/U/g; s/è/e/g' > cod_pp_procesado
-
 rm cod_pp_bruto
 rm cod_pp_preprocesado
 
 # Separamos ordenadamente los codigos y provincias, -o toma solo la coincidencia y no la linea entera
 cat cod_pp_procesado | grep -Eo "[0-9]+" > cod_p_n
 cat cod_pp_procesado | grep -Eo "[^0-9]+" > cod_p_paux
-rm cod_pp
+rm cod_pp_procesado
 
 # Eliminar espacios y barras de los nombres
 cat cod_p_paux | cat cod_p_paux |  sed " s/\//_/g; s/ //g" > pp
